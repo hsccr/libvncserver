3b7c4154c36e290554def9829c7e5fffb3e41922
Chungrae Cho <ccr@emstone.com>
2025-06-18 17:47:37 +0900

    tls_openssl: add error logging when SSL_CTX_new() fails
    SSL_CTX_new() 함수 실패할 때
    로그를 기록합니다.

--- src/libvncclient/tls_openssl.c
+++ src/libvncclient/tls_openssl.c
@@ -263,7 +263,16 @@ open_ssl_connection (rfbClient *client, int sockfd, rfbBool anonTLS, rfbCredenti
 
   if (!(ssl_ctx = SSL_CTX_new(SSLv23_client_method())))
   {
+    unsigned long err_no;
+
     rfbClientLog("Could not create new SSL context.\n");
+    while((err_no = ERR_get_error()) != 0)
+    {
+      char err_buf[256];
+
+      ERR_error_string_n(err_no, err_buf, sizeof(err_buf));
+      rfbClientLog("\t error: %s(%d)\n", err_buf, err_no);
+    }
     return NULL;
   }
 
